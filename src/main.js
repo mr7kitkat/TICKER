eadc17f9d1a369885f5d1c8837c9c5ba5dba1039
mr7kitkat <mr7kitkat@gmail.com>
2025-02-18 08:54:40 +0530

    made some basic ui skeleton

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
+import "./styles/style.css"
 import todo from "./scripts/todo";
 
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".hamburger").parentElement.classList.toggle("active")
+})
